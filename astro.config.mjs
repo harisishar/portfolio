8dda002805981e6c9525f963ab286e094287212f
harisishar <harisishar@gmail.com>
2023-05-17 01:15:21 +0800

    feature img

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: "https://harisishar.netlify.com",
   integrations: [
     tailwind(),
     sitemap({
